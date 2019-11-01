b483cb88a9562b7c2a00b0496677aced4b7fc36a
hramadan <hramadan@sumerge.com>
2019-11-01 22:44:57 +0200

    Updates

---
+++ precache-manifest.e451178d1228eb73ce234928d630d3eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f6fbdfa209b80cc470a4bec98366be",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "f8b5bcc023b94b249f48",
+    "url": "/pokedex/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d6560bd4c4c773c9c964",
+    "url": "/pokedex/static/css/main.d0ea62c7.chunk.css"
+  },
+  {
+    "revision": "f8b5bcc023b94b249f48",
+    "url": "/pokedex/static/js/2.b588511c.chunk.js"
+  },
+  {
+    "revision": "d6560bd4c4c773c9c964",
+    "url": "/pokedex/static/js/main.f50c2f62.chunk.js"
+  },
+  {
+    "revision": "27f16e0463aa7cf68513",
+    "url": "/pokedex/static/js/runtime-main.dc93633d.js"
+  }
+]);
