948cd17a2ce78e7696fea8e82adf3a46f8ccc985
HatemRamadan <hatemram96@gmail.com>
2020-02-18 00:52:33 +0200

    Added redux and converted files to typescript

---
+++ build/precache-manifest.61ee4faebe529daadf067f94fc1a6ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f9d043cee7ea19fa7777909b44f700",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "4b797864adbbaa44eafb",
+    "url": "/pokedex/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b4edec1d58523c3f9ed4",
+    "url": "/pokedex/static/css/main.39f1501f.chunk.css"
+  },
+  {
+    "revision": "4b797864adbbaa44eafb",
+    "url": "/pokedex/static/js/2.31380ec3.chunk.js"
+  },
+  {
+    "revision": "b4edec1d58523c3f9ed4",
+    "url": "/pokedex/static/js/main.fecddda3.chunk.js"
+  },
+  {
+    "revision": "27f16e0463aa7cf68513",
+    "url": "/pokedex/static/js/runtime-main.dc93633d.js"
+  }
+]);
